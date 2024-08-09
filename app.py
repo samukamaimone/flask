941c5aacdaf3eff861c1d3300a77e74f78647bca
samukamaimone <samuelmaimone55@gmail.com>
2024-08-09 08:36:46 -0300

    aula de 09/08

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__) 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/aula')
+@app.route('/aula/<nome>')
+@app.route('/aula/<nome>/<curso>')
+@app.route('/aula/<nome>/<curso>/<int:ano>')
+def aula(nome = 'Joao', curso = 'Infomatica', ano = 1):
+    dados = {'nome':nome,'curso':curso,'ano':ano}
+    return render_template('aula.html', dados_curso = dados)
+
+if __name__ == '__main__':
+    app.run()
