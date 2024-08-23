9df17eea07f7d63f214f2050fd7f9bb42282ac44
samukamaimone <samuelmaimone55@gmail.com>
2024-08-23 08:40:36 -0300

    aula de 23/08

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request
-
 app = Flask(__name__)
-app.config['SECRET_KEY']
+from database import db
+from flask_migrate import Migrate
+from models import Usuario
+app.config['SECRET_KEY'] = ['2c1685c157fe2450fda443a0753bb69f']
+
+# drive://usuario:senha@servidor/banco_de_dados
+conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/flaskg2"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
@@ -21,8 +31,14 @@ def form():
 
 @app.route('/dados', methods=['POST'])
 def dados():
+    flash('Dados enviados!')
     dados = request.form
     return render_template('dados.html', dados=dados)
+
+@app.route('/usuario')
+def usuario():
+    u = Usuario.query.all()
+    return render_template('usuario_lista.html', dados=u)
 
 if __name__ == '__main__':
     app.run()
